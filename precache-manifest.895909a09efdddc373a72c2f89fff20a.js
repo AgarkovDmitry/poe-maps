1a8156e01800e67236a9236db61b2adc7ec38bb9
Dmitry Aharkov <daharkov@mobilunity.com>
2019-03-21 08:13:51 +0200

    Updates

---
+++ precache-manifest.895909a09efdddc373a72c2f89fff20a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d329b7feca7de07e348",
+    "url": "/poe-maps/static/js/runtime~main.9029ae6b.js"
+  },
+  {
+    "revision": "4c1fe61134d7c7470ca0",
+    "url": "/poe-maps/static/js/main.f4b2c131.chunk.js"
+  },
+  {
+    "revision": "6da307b70deff093b795",
+    "url": "/poe-maps/static/js/2.99ec6c6a.chunk.js"
+  },
+  {
+    "revision": "4c1fe61134d7c7470ca0",
+    "url": "/poe-maps/static/css/main.40befaad.chunk.css"
+  },
+  {
+    "revision": "bc71e4a260c469df890bee47bb4c27e9",
+    "url": "/poe-maps/index.html"
+  }
+];
